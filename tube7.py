5409eef246b2c7a587a2b1941cd00d94fefef4f9
hawa200 <85688830+hawa200@users.noreply.github.com>
2021-07-21 22:43:33 +0530

    Add files via upload
    .variable, data types, and  typecasting

---
+++ tube7.py
@@ -0,0 +1,34 @@
+print("hello ") # if some body wants to print a full paragraph in this state he/she will not write whole paragraph in print statement
+var1 = '''i am Dr.Hacker and i want to hack some thing 
+my interest is in programming, singing, playing video games, and in cyber security''' # is an string variable
+var2 = 456  # is an intiger variable
+var3 = 5.8  # floating point variable
+var4 = "dr.hacker "
+var5 = "how are you"
+print(var1) # so as you have seen variables are particularly containers in which we can store our data
+  
+print(type(var1))
+print(type(var2))
+print(type(var3))
+
+print(var2 + var3)
+
+#print(var1 + var3) 
+print(var4 + var5)
+
+print("======================================================================================================")
+ 
+ #how to change string to integer?
+var6 = "75"
+var7 = "85"
+
+
+print(int(var6) + int(var7))
+
+''' 
+str()
+int()
+float()
+'''
+print("======================================================================================================")
+print(10 * "dr.hacker\n  ")
