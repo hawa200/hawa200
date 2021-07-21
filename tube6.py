66798d6a0c216d58a555bdcc09851c3ccc29ab97
hawa200 <85688830+hawa200@users.noreply.github.com>
2021-07-21 20:14:35 +0530

    Add files via upload

---
+++ tube6.py
@@ -0,0 +1,12 @@
+#please do not remove this line 
+#print("I am Dr.Hacker")
+'''multi 
+line 
+ignore 
+now'''
+#print('''we are enormous!
+#from dark
+#we are 2 or the whole world
+#get lost...''')
+#print("c:\'Dr.Hacker")
+print(" Dr.Hacker is \n is an hacker \t1")
